e8bedfbf00029b832257f97cdecbcc7b214cc6d3
Toh Jia Xuan <jiaxuan.toh10@rafflesgirlssch.edu.sg>
2023-06-24 15:14:09 +0800

    Added tabview and organised into different tabs

---
+++ ComplimentsView.swift
@@ -0,0 +1,20 @@
+//
+//  ComplimentsView.swift
+//  Quirky Compliments Make Me Happy!!!
+//
+//  Created by RGS on 24/6/23.
+//
+
+import SwiftUI
+
+struct ComplimentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ComplimentsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComplimentsView()
+    }
+}
---
+++ PetView.swift
@@ -0,0 +1,20 @@
+//
+//  PetView.swift
+//  Quirky Compliments Make Me Happy!!!
+//
+//  Created by RGS on 24/6/23.
+//
+
+import SwiftUI
+
+struct PetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PetView_Previews: PreviewProvider {
+    static var previews: some View {
+        PetView()
+    }
+}

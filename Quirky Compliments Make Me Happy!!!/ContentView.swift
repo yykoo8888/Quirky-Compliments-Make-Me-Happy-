224d2419ac1d701da73143c6ebb3fb295a05bd11
Yun Ye <yunye.koo10@rafflesgirlssch.edu.sg>
2023-06-24 12:05:40 +0800

    Initial Commit

---
+++ Quirky Compliments Make Me Happy!!!/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Quirky Compliments Make Me Happy!!!
+//
+//  Created by Koo  on 24/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
